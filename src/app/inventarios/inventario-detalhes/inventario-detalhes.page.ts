bb8864f2b7df441aaba228196c9672c387ee5c93
Paulo Estêvão <paulo.estevao@tst.jus.br>
2019-12-03 19:13:49 -0300

    Gráfico dos detalhes do inventário

--- src/app/inventarios/inventario-detalhes/inventario-detalhes.page.ts
+++ src/app/inventarios/inventario-detalhes/inventario-detalhes.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, NavigationExtras } from '@angular/router';
 import { Inventario } from '../inventario';
 import { Bem } from '../bem';
+import { GoogleChartInterface } from 'ng2-google-charts/google-charts-interfaces';
+import { InventariosService } from '../inventarios.service';
 
 @Component({
   selector: 'app-inventario-detalhes',
@@ -11,8 +13,13 @@ import { Bem } from '../bem';
 export class InventarioDetalhesPage implements OnInit {
 
   inventario: Inventario;
+  grafico: GoogleChartInterface;
 
-  constructor(private route: ActivatedRoute, private router: Router) { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private service: InventariosService
+  ) { }
 
   ngOnInit() {
     this.route.queryParams.subscribe(params => {
@@ -20,6 +27,26 @@ export class InventarioDetalhesPage implements OnInit {
         this.inventario = this.router.getCurrentNavigation().extras.state.inventario;
       }
     });
+
+    this.criarGrafico();
+  }
+
+  criarGrafico() {
+    this.grafico = {
+      chartType: 'PieChart',
+      dataTable: [
+        ['Bens', 'Quantidade de bens'],
+        ['Conferidos', this.qtdeBensConferidos],
+        ['Não conferidos', this.qtdeTotalBens - this.qtdeBensConferidos]
+      ],
+      options: {
+        title: 'Bens',
+        width: '100%',
+        colors: ['green', 'red'],
+        pieHole: 0.4,
+        legend: 'top'
+      },
+    };
   }
 
   abrirListaBens() {
@@ -27,6 +54,10 @@ export class InventarioDetalhesPage implements OnInit {
       state: { bens: this.inventario.bens }
     };
     this.router.navigate(['tabs/inventarios/bens'], navigationExtras);
+  }
+
+  tornarAtual() {
+    this.inventario = this.service.definirInventarioAtual(this.inventario);
   }
 
   get bensConferidos(): Bem[] {
